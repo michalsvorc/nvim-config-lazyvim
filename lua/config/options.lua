59227b684cd0c1f327e5a59ecaeeb008546d4bb3
Michal Svorc <m.svorc@sportradar.com>
2023-12-21 06:34:06 +0100

    feat(options): enable spell check by default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.wo.conceallevel = 0
+
+-- Spelling
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
