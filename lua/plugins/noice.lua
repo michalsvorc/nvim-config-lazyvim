84f4efccad28a869eb59ff5371b2756ca65d0785
Michal Svorc <m.svorc@sportradar.com>
2024-03-27 20:38:07 +0100

    chore(noice): disable popup menu

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,5 +5,8 @@ return {
       enabled = true, -- enables the Noice cmdline UI
       view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
     },
-  }
+    popupmenu = {
+      enabled = false, -- enables the Noice popupmenu UILeave
+    },
+  },
 }
