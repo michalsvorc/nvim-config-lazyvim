79b04dba49c47a8440a311ae301844fd1001b290
Michal Svorc <m.svorc@sportradar.com>
2024-03-27 18:14:52 +0100

    chore(noice): move command line to bottom

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    cmdline = {
+      enabled = true, -- enables the Noice cmdline UI
+      view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+    },
+  }
+}
