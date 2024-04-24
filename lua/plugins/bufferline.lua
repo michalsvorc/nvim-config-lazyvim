6b99b96569cfc1b53d1c51bbb879115c5686650d
Michal Svorc <dev@michalsvorc.com>
2024-04-24 16:15:07 +0200

    fix(bufferline): pin down v4.5.3
    Fixes https://github.com/akinsho/bufferline.nvim/issues/895

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,5 @@
+-- https://github.com/akinsho/bufferline.nvim/releases/tag/v4.5.3
+--- Last working before https://github.com/akinsho/bufferline.nvim/issues/895.
+--- Test with editing a file in floating terminal and flatten.
+return { "akinsho/bufferline.nvim", tag = "v4.5.3"}
+
