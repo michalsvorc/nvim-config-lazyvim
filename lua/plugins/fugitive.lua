dba8c4f24419346a6c706736407da5cbf558eee8
Michal Svorc <dev@michalsvorc.com>
2024-05-22 21:26:56 +0200

    feat(git): switch from neogit to fugitive

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,49 @@
+---@type LazySpec
+return {
+  "tpope/vim-fugitive",
+  lazy = false,
+  keys = {
+    {
+      "<leader>gg",
+      function()
+        vim.cmd("vert Git")
+      end,
+      desc = "Git menu",
+    },
+    {
+      "<leader>gc",
+      function()
+        vim.cmd("vert Git commit")
+      end,
+      desc = "Commit",
+    },
+    {
+      "<leader>gl",
+      function()
+        vim.cmd("vert Git log")
+      end,
+      desc = "Log",
+    },
+    {
+      "<leader>gL",
+      function()
+        vim.cmd("vert Git log -- %")
+      end,
+      desc = "Log current",
+    },
+    {
+      "<leader>gs",
+      function()
+        vim.cmd("Git status")
+      end,
+      desc = "Status",
+    },
+    {
+      "<leader>gd",
+      function()
+        vim.cmd("Gvdiffsplit")
+      end,
+      desc = "Diff",
+    },
+  },
+}
