2316183fadf251be43c443865a64d70e99fa9c37
Michal Svorc <dev@michalsvorc.com>
2024-05-31 13:30:31 +0200

    feat(plugins): remove nvim-lualine/lualine.nvim
    Throwing errors with telescope and command window.

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
 }
