e334063b246dd3852247fa36df80f81284833526
Michal Svorc <dev@michalsvorc.com>
2024-07-21 16:20:04 +0200

    chore(plugins): spectre removed from defaults
    https://github.com/LazyVim/LazyVim/commit/0d561a3226b46f6a44c4f2c9be2288f3b52cc351#diff-7cca712fe17a323d19a68b2ea48416807d822df291c4d811466e01bd398b0278

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
-  { "nvim-pack/nvim-spectre", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
