1edb3d96972cc29deadba2ca8931e82e1ac43139
Michal Svorc <dev@michalsvorc.com>
2024-07-21 17:00:45 +0200

    feat(grug): add custom keymaps

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -11,4 +11,54 @@ return {
       },
     })
   end,
+  keys = {
+    {
+      "<leader>r",
+      function()
+        require("grug-far").grug_far()
+      end,
+      mode = "n",
+      desc = "FaR",
+    },
+    {
+      "<leader>r",
+      function()
+        local opts = {
+          prefills = {
+            search = vim.fn.expand("<cword>"),
+          },
+        }
+        require("grug-far").grug_far(opts)
+      end,
+      mode = "v",
+      desc = "FaR (selection)",
+    },
+    {
+      "<leader>R",
+      function()
+        local opts = {
+          prefills = {
+            flags = vim.fn.expand("%"),
+          },
+        }
+        require("grug-far").grug_far(opts)
+      end,
+      mode = "n",
+      desc = "FaR (current buffer)",
+    },
+    {
+      "<leader>R",
+      function()
+        local opts = {
+          prefills = {
+            search = vim.fn.expand("<cword>"),
+            flags = vim.fn.expand("%"),
+          },
+        }
+        require("grug-far").grug_far(opts)
+      end,
+      mode = "v",
+      desc = "FaR (selection, current buffer)",
+    },
+  },
 }
